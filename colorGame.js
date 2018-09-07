4551da964b4f32afa1c7d93e20251114bae3acd9
G1nn <luxxx917@umn.edu>
2018-09-06 20:20:09 -0500

    Initial commit

---
+++ colorGame.js
@@ -0,0 +1,52 @@
+var colors = generateRandomColors(6);
+var squares = document.querySelectorAll(".square");
+var picked = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var reset = document.querySelector('#reset');
+
+colorDisplay.textContent = picked;
+
+for (var i = 0; i < squares.length; i++){
+	//add initial colors
+	squares[i].style.backgroundColor = colors[i];
+	//add click listeners
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor;
+		if (clickedColor === picked){
+			messageDisplay.textContent = "Correct";
+			changeColors(squares, picked);
+			h1.style.backgroundColor = picked;
+		} else {
+			messageDisplay.textContent = "Try Again";
+			this.style.backgroundColor = "#232323";
+		}
+	})	
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function generateRandomColors(num){
+	var arr = [];
+	for (var i = 0; i < num; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function pickColor(){
+	return colors[Math.floor(Math.random() * squares.length)];
+}
+
+function changeColors(blocks, color){
+	for(var i = 0; i < blocks.length; i++){
+		console.log("h");
+		blocks[i].style.backgroundColor = color;
+	}
+}
